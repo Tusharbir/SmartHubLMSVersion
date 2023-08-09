ccfbe3771efde4d9ee1274370876e08e6a4bbae1
Tusharbir <tusharbirmutty07@gmail.com>
2023-08-10 01:00:47 +0530

    redsesign done

--- app/src/main/java/cseb/tech/smarthublms/AdminHomePage.java
+++ app/src/main/java/cseb/tech/smarthublms/AdminHomePage.java
@@ -4,15 +4,49 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 public class AdminHomePage extends AppCompatActivity {
+    private FirebaseFirestore db;
 
     private Toolbar appbar;
+    private TextView welcomeAdminName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home_page);
         appbar=findViewById(R.id.AppBar);
+
+        db=FirebaseFirestore.getInstance();
+
         setSupportActionBar(appbar);
+        welcomeAdminName=findViewById(R.id.welcomeAdminText);
+
+        String username = getIntent().getStringExtra("USER_NAME");
+//        welcomeAdminName.setText(username);
+        fetchNameFromFirestore(username);
     }
+
+    private void fetchNameFromFirestore(String username) {
+        db.collection("users")
+                .whereEqualTo("username", username) // Assuming the field is named "username"
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            String name = document.getString("Name");
+                            welcomeAdminName.setText("WELCOME "+name.toUpperCase());
+                            Log.i("username",name);
+                        }
+                    } else {
+                        Log.d("NextActivity", "Error getting documents: ", task.getException());
+                    }
+                });
+
+    }
+
 }
--- app/src/main/java/cseb/tech/smarthublms/MainActivity.java
+++ app/src/main/java/cseb/tech/smarthublms/MainActivity.java
@@ -4,17 +4,63 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar appbar;
+
+    private EditText userText, passText;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         appbar=findViewById(R.id.AppBar);
+//        getSupportActionBar().setTitle(R.string.app_name);
         setSupportActionBar(appbar);
+
+
+        userText=findViewById(R.id.userNameTv);
+        passText=findViewById(R.id.passwordTv);
+        mAuth=FirebaseAuth.getInstance();
+
+    }
+
+    public void logicButtonLogin(View view){
+        String username = userText.getText().toString();
+        String password = passText.getText().toString();
+        loginUser(username, password);
+    }
+
+    private void loginUser(String username, String password){
+        mAuth.signInWithEmailAndPassword(username, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        openNextActivity(username);
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(MainActivity.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+    private void openNextActivity(String username){
+        Intent intent = new Intent(this, AdminHomePage.class);
+        intent.putExtra("USER_NAME", username);
+        startActivity(intent);
+
     }
 }
